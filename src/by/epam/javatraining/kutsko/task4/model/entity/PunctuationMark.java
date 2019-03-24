3ebc8fd64c092b05bf1168ccee9ab26ed11c2df0
Kate <kayara998@gmail.com>
2019-03-24 19:30:43 +0300

    Added validation to constructor

--- src/by/epam/javatraining/kutsko/task4/model/entity/PunctuationMark.java
+++ src/by/epam/javatraining/kutsko/task4/model/entity/PunctuationMark.java
@@ -1,9 +1,14 @@
 package by.epam.javatraining.kutsko.task4.model.entity;
 
+import by.epam.javatraining.kutsko.task4.util.validator.Validator;
+
 public class PunctuationMark extends SimpleTextUnit implements Cloneable {
 	
 	public PunctuationMark(String contents) {
-		super(contents);
+		super();
+		if (Validator.validateAsPunctuationMark(contents)) {
+			setContents(contents);
+		}
 	}
 	
 	public PunctuationMark() {
