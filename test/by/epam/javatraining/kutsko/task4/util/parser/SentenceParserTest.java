ec0308b9d8bd308c3554fed5605697b495e72b71
Kate <kayara998@gmail.com>
2019-03-24 19:27:00 +0300

    Added sentence parser tests

---
+++ test/by/epam/javatraining/kutsko/task4/util/parser/SentenceParserTest.java
@@ -0,0 +1,47 @@
+package by.epam.javatraining.kutsko.task4.util.parser;
+
+import org.testng.annotations.Test;
+
+import by.epam.javatraining.kutsko.task4.model.entity.*;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SentenceParserTest {
+	
+	@Test
+	public void testCreatingSentence() {
+		Map<Integer, SimpleTextUnit> map = new HashMap<>();
+		map.put(0, new Word("This"));
+		map.put(1, new Word("is"));
+		map.put(2, new Word("a"));
+		map.put(3, new Word("sentence"));
+		map.put(4, new PunctuationMark("."));
+		Sentence expected = new Sentence(map);
+		
+		Sentence actual = new SentenceParser().create("This is a sentence.");
+		
+		assertEquals(actual, expected);
+	}
+	
+
+	@Test 
+	public void testCreatingSentenceFromNull() {
+		Sentence expected = new Sentence();
+		
+		Sentence actual = new SentenceParser().create(null);
+		
+		assertEquals(actual, expected);
+	}
+	
+	@Test
+	public void testCreatingSentenceFronInvalidString() {
+		Sentence expected = new Sentence();
+		
+		Sentence actual = new SentenceParser().create("this is not a sentence.");
+		
+		assertEquals(actual, expected);
+	}
+}
