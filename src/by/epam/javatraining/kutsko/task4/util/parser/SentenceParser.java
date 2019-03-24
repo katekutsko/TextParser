dd0413027aeb531c1e02873af550e48a4f707930
Kate <kayara998@gmail.com>
2019-03-24 19:28:27 +0300

    Changed sentence parser

--- src/by/epam/javatraining/kutsko/task4/util/parser/SentenceParser.java
+++ src/by/epam/javatraining/kutsko/task4/util/parser/SentenceParser.java
@@ -19,31 +19,35 @@ public class SentenceParser extends AbstractParser {
 	public Sentence create(String sentence) {
 
 		Sentence newSentence = new Sentence();
-		Pattern pattern = Pattern.compile(SENTENCE_DELIMETER);
-		Matcher matcher = pattern.matcher(sentence);
-		List<String> sentenceFragments = new ArrayList<String>();
+		
+		if (sentence != null && Validator.validateAsSentence(sentence)) {
+	
+			Pattern pattern = Pattern.compile(SENTENCE_DELIMETER);
+			Matcher matcher = pattern.matcher(sentence);
+			List<String> sentenceFragments = new ArrayList<String>();
 
-		while (matcher.find()) {
-			sentenceFragments.add(matcher.group());
-		}
-
-		for (String sentenceFragment : sentenceFragments) {
-
-			if (Validator.validateAsPunctuationMark(sentenceFragment)) {
-				
-				if (!newSentence.isEmpty()) {
-					
-					nextParser = punctuationMarkParser;
-					SimpleTextUnit unit = (SimpleTextUnit) nextParser.create(sentenceFragment);
-					newSentence.add(unit);
-				}
-
-			} else {
-
-				nextParser = wordParser;
-				newSentence.add(nextParser.create(sentenceFragment));
+			while (matcher.find()) {
+				sentenceFragments.add(matcher.group());
 			}
 
+			for (String sentenceFragment : sentenceFragments) {
+
+				if (Validator.validateAsPunctuationMark(sentenceFragment)) {
+
+					if (!newSentence.isEmpty()) {
+
+						nextParser = punctuationMarkParser;
+						SimpleTextUnit unit = (SimpleTextUnit) nextParser.create(sentenceFragment);
+						newSentence.add(unit);
+					}
+
+				} else {
+
+					nextParser = wordParser;
+					newSentence.add(nextParser.create(sentenceFragment));
+				}
+
+			}
 		}
 		return newSentence;
 	}
