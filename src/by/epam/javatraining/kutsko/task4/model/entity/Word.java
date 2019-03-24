46c7a01b05372cc38426924a3229cfc7347d3e4e
Kate <kayara998@gmail.com>
2019-03-24 17:42:57 +0300

    Added copying constructor

--- src/by/epam/javatraining/kutsko/task4/model/entity/Word.java
+++ src/by/epam/javatraining/kutsko/task4/model/entity/Word.java
@@ -1,15 +1,22 @@
 package by.epam.javatraining.kutsko.task4.model.entity;
 
 public class Word extends SimpleTextUnit implements Cloneable {
-	
+
 	public Word(String contents) {
 		super(contents);
 	}
-	
+
 	public Word() {
 		super();
 	}
-	
+
+	public Word(Word word) {
+		super();
+		if (word != null && word.contents != null) {
+			contents = word.contents;
+		}
+	}
+
 	@Override
 	public Word clone() {
 		return new Word(contents);
