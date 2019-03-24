d398d91d51afd189c934248f1d94f35e60a26478
Kate <kayara998@gmail.com>
2019-03-24 17:42:00 +0300

    Added reversing method

--- src/by/epam/javatraining/kutsko/task4/model/logic/TextAnalyzer.java
+++ src/by/epam/javatraining/kutsko/task4/model/logic/TextAnalyzer.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.apache.log4j.Logger;
+
 import by.epam.javatraining.kutsko.task4.model.entity.*;
 import by.epam.javatraining.kutsko.task4.model.exception.CorruptParameterReferenceException;
 import by.epam.javatraining.kutsko.task4.model.exception.NoSuchTextFragmentException;
@@ -16,6 +18,12 @@ import by.epam.javatraining.kutsko.task4.model.exception.NoSuchTextFragmentExcep
  * @version 1.0 21.03.19
  */
 public class TextAnalyzer {
+
+	private static final Logger LOGGER;
+
+	static {
+		LOGGER = Logger.getRootLogger();
+	}
 
 	public static String getAllWordsOfTextInAlphabeticOrder(Text text) {
 
@@ -29,15 +37,12 @@ public class TextAnalyzer {
 				if (textBlock instanceof Paragraph) {
 
 					Paragraph paragraph = (Paragraph) textBlock;
-
 					paragraph.getAllFragments().forEach((sentenceNumber, sentence) -> {
 
 						Sentence currentSentence = (Sentence) sentence;
-
 						currentSentence.getAllFragments().forEach((sentencePartNumber, sentencePart) -> {
 
 							if (sentencePart instanceof Word) {
-
 								wordList.add(sentencePart.toString().toLowerCase());
 							}
 						});
@@ -61,63 +66,158 @@ public class TextAnalyzer {
 			}
 			return builder.toString();
 		}
+
+		LOGGER.info("Reference to text was null");
 		return "";
 	}
 
-	public static String reverseSentences(Text text) {
+	private static List<Sentence> getSentences(Text text) {
+
+		List<Sentence> sentenceList = new ArrayList<>();
+
+		text.getAllFragments().forEach((textBlockNumber, textBlock) -> {
+
+			if (textBlock instanceof Paragraph) {
+				Paragraph paragraph = (Paragraph) textBlock;
+
+				paragraph.getAllFragments().forEach((sentenceNumber, sentence) -> {
+					sentenceList.add((Sentence) sentence.clone());
+				});
+			}
+		});
+		return sentenceList;
+	}
+
+	private static String buildString(List<Sentence> sentenceList) {
+
+		StringBuilder builder = new StringBuilder();
+
+		for (Sentence sentence : sentenceList) {
+			builder.append(sentence.toString()).append("\n");
+		}
+		return builder.toString();
+	}
+
+	public static String swapFirstAndLast(Text text) {
 
 		if (text != null) {
 
-			List<Sentence> sentenceList = new ArrayList<>();
-
-			text.getAllFragments().forEach((textBlockNumber, textBlock) -> {
-
-				if (textBlock instanceof Paragraph) {
-
-					Paragraph paragraph = (Paragraph) textBlock;
-
-					paragraph.getAllFragments().forEach((sentenceNumber, sentence) -> {
-						sentenceList.add((Sentence) sentence.clone());
-					});
-				}
-			});
+			List<Sentence> sentenceList = getSentences(text);
 
 			for (Sentence sentence : sentenceList) {
 
 				try {
 
 					int lastIndex = sentence.getAmountOFragments() - 1;
+					
 					Word firstWord = (Word) sentence.getElement(0);
 					firstWord.setContents(firstWord.toString().toLowerCase());
+					
 					TextUnit unit = sentence.getElement(lastIndex);
 
 					if (!(unit instanceof Word)) {
 						unit = sentence.getElement(--lastIndex);
 					}
-					
+
 					Word lastWord = (Word) unit;
-					lastWord.setContents(
-					lastWord.toString().substring(0, 1).toUpperCase() +
-					lastWord.toString().substring(1));
 
 					sentence.replace(0, lastWord);
 					sentence.replace(lastIndex, firstWord);
 
+					firstWord.setContents(
+							firstWord.toString().substring(0, 1).toUpperCase() + lastWord.toString().substring(1));
+					lastWord.setContents(lastWord.toString().toLowerCase());
+
 				} catch (NoSuchTextFragmentException e) {
-					// TODO
-					e.printStackTrace();
+					LOGGER.info("Nonexistent text fragment was addressed");
 				} catch (CorruptParameterReferenceException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					LOGGER.info("Reference to parameter was null");
 				}
 			}
+			return buildString(sentenceList);
 
-			StringBuilder builder = new StringBuilder();
-			
+		} else {
+			LOGGER.info("Reference to text was null");
+		}
+		return "";
+	}
+
+	public static String reverseWordsInSentences(Text text) {
+
+		if (text != null) {
+
+			List<Sentence> sentenceList = getSentences(text);
+
 			for (Sentence sentence : sentenceList) {
-				builder.append(sentence.toString()).append("\n");
+
+				try {
+
+					TextUnit check = null;
+					Word firstWord = null;
+					Word secondWord = null;
+
+					int previousFirstWordIndex = -1;
+					int previousSecondWordIndex = sentence.getAmountOFragments();
+					int middle = previousSecondWordIndex / 2;
+
+					boolean stop = false;
+
+					while (!stop) {
+
+						int i = previousFirstWordIndex;
+
+						while (i < middle) {
+							i++;
+							previousFirstWordIndex = i;
+							check = sentence.getElement(previousFirstWordIndex);
+
+							if (check instanceof Word) {
+								firstWord = (Word) check;
+								break;
+							}
+						}
+
+						int j = previousSecondWordIndex;
+
+						while (j >= middle) {
+
+							j--;
+							previousSecondWordIndex = j;
+							check = sentence.getElement(previousSecondWordIndex);
+
+							if (check instanceof Word) {
+								secondWord = (Word) check;
+								break;
+							}
+						}
+
+						sentence.replace(previousFirstWordIndex, secondWord);
+						sentence.replace(previousSecondWordIndex, firstWord);
+						if (i >= j) {
+							stop = true;
+						}
+					}
+					firstWord = (Word) sentence.getElement(0);
+
+					if ((check = sentence.getElement(sentence.getAmountOFragments() - 1)) instanceof Word) {
+						secondWord = (Word) check;
+					} else {
+						secondWord = (Word) sentence.getElement(sentence.getAmountOFragments() - 2);
+					}
+
+					firstWord.setContents(
+							firstWord.toString().substring(0, 1).toUpperCase() + firstWord.toString().substring(1));
+					secondWord.setContents(secondWord.toString().toLowerCase());
+
+				} catch (NoSuchTextFragmentException e) {
+					LOGGER.info("Nonexistent text fragment was addressed");
+				} catch (CorruptParameterReferenceException e) {
+					LOGGER.info("Reference to parameter was null");
+				}
 			}
-			return builder.toString();
+			return buildString(sentenceList);
+		} else {
+			LOGGER.warn("Reference to text was null");
 		}
 		return "";
 	}
