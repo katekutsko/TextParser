48bfe5c7f2031b2bbb230fef355b39983090a1ac
Kate <kayara998@gmail.com>
2019-03-24 19:26:37 +0300

    Added sentence validation

--- src/by/epam/javatraining/kutsko/task4/util/validator/Validator.java
+++ src/by/epam/javatraining/kutsko/task4/util/validator/Validator.java
@@ -7,15 +7,21 @@ public class Validator {
 	public static final String PARAGRAPH_REGEXP = "\\t[A-Z]+[\\w+\\p{Punct}+\\s+]+[.:]";
 	public static final String PUNCTUATION_MARK_REGEXP = "\\p{Punct}+|\\t";
 	public static final String CODE_BLOCK_REGEXP = "(^(.*\\n?).*\\{(?s).*\\})|(^\\s?.+ = .+;\\n*)+";
+	public static final String SENTENCE_REGEXP = "^[A-Z0-9]+[\\w\\s]+[\\p{Punct}]?$";
+	public static final String WORD_REGEXP = "\\w+";
 	private static Pattern paragraphPattern;
 	private static Pattern punctuationPattern;
 	private static Pattern codeBlockPattern;
+	private static Pattern wordPattern;
+	private static Pattern sentencePattern;
 
 	static {
 
 		paragraphPattern = Pattern.compile(PARAGRAPH_REGEXP);
 		punctuationPattern = Pattern.compile(PUNCTUATION_MARK_REGEXP);
 		codeBlockPattern = Pattern.compile(CODE_BLOCK_REGEXP);
+		wordPattern = Pattern.compile(WORD_REGEXP);
+		sentencePattern = Pattern.compile(SENTENCE_REGEXP);
 	}
 
 	public static boolean validateAsParagraph(String paragraph) {
@@ -35,6 +41,20 @@ public class Validator {
 	public static boolean validateAsCodeBlock(String code) {
 		if (code != null) {
 			return codeBlockPattern.matcher(code).matches();
+		}
+		return false;
+	}
+	
+	public static boolean validateAsWord(String word) {
+		if (word != null) {
+			return wordPattern.matcher(word).matches();
+		}
+		return false;
+	}
+	
+	public static boolean validateAsSentence(String sentence) {
+		if (sentence != null) {
+			return sentencePattern.matcher(sentence).matches();
 		}
 		return false;
 	}
