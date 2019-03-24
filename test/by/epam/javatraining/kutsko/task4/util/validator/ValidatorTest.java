cb4c9b06cace34cdfffb12a0857bbb191a721ecc
Kate <kayara998@gmail.com>
2019-03-24 17:49:39 +0300

    Added code block validation

--- test/by/epam/javatraining/kutsko/task4/util/validator/ValidatorTest.java
+++ test/by/epam/javatraining/kutsko/task4/util/validator/ValidatorTest.java
@@ -10,28 +10,26 @@ public class ValidatorTest {
 	private String testString;
 	private boolean expected;
 	private String testSymbols;
+	private String testCodeBlock;
 	
 	@Test(dataProvider = "dp")
-	public void f(String v, String n, Boolean s) {
+	public void f(String p, String v, String n, Boolean s) {
+		testCodeBlock = p;
 		testSymbols = v;
 		testString = n;
 		expected = s;
 	}
 
-	@BeforeMethod
-	public void beforeMethod() {
-	}
-
   @DataProvider
   public Object[][] dp() {
     return new Object[][] {
-      new Object[] {"sdgdf", "89this is an example of an invalid paragraph. \nIt is very short, "
+      new Object[] {"GGGG", "sdgdf", "89this is an example of an invalid paragraph. \nIt is very short, "
 				+ "but contains several sentences and \n has logical completion;", false },
-      new Object[] {"??????", "FGDHKESKTY%#Y#RWGebhdnlkcnjc;ouYD98W7D98S9EFKJM FKVMSDLVK FCSV.", false },
-      new Object[] { ",", "\tThis is an example of a paragraph. It is very short, "
+      new Object[] {"list = new ArrayList<>()", "??????", "FGDHKESKTY%#Y#RWGebhdnlkcnjc;ouYD98W7D98S9EFKJM FKVMSDLVK FCSV.", false },
+      new Object[] {"list = new ArrayList<>();", ",", "\tThis is an example of a paragraph. It is very short, "
 				+ "but \ncontains several sentences and \n has logical completion.", true },
-      new Object[] { " ", "\t .", false },
-      new Object[] { null, null, false }
+      new Object[] {" ", " ", "\t .", false },
+      new Object[] {null, null, null, false }
     };
   }
 
@@ -47,4 +45,10 @@ public class ValidatorTest {
 		Assert.assertEquals(actual, expected);
 	}
 
+	@Test
+	public void testCodeBlockValidation() {
+		boolean actual = Validator.validateAsPunctuationMark(testSymbols);
+		Assert.assertEquals(actual, expected);
+	}
+	
 }
